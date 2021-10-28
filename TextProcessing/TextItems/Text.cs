6728b3e72e2424d9d6f2bcfc1a468b7983e340fe
Ilya Bondarik <il.bandaryk@gmail.com>
2021-10-29 00:41:05 +0300

    updated SortByWordCount

--- TextProcessing/TextItems/Text.cs
+++ TextProcessing/TextItems/Text.cs
@@ -12,20 +12,36 @@ namespace TextProcessing.TextItems
         {
             Sentences = TextParser.GetSentences(text);
         }
+
         internal Text(string[] text) : this(string.Join(" ", text)) { }
 
         public override string ToString()
         {
             var textBuilder = new StringBuilder();
-            foreach (var sentence in Sentences) textBuilder.Append(sentence.ToString() + ' ');
+            foreach (var sentence in Sentences) textBuilder.Append(sentence.ToString());
             return textBuilder.ToString();
         }
 
         public List<Sentence> SortByWordCount()
         {
-            var sortedSentences = new SortedList<int, Sentence>(Sentences.Count);
-            foreach (var sentence in Sentences) sortedSentences.Add(sentence.Words.Count, sentence);
-            return sortedSentences.Values.ToList();
+            var sortedSentences = new List<Sentence>(Sentences.Count);
+            var wordsCount = new List<int>(Sentences.Count);
+
+            foreach (var sentence in Sentences)
+            {
+                wordsCount.Add(sentence.Words.Count);
+            }
+            wordsCount.Sort();
+
+            foreach (var wordCount in wordsCount)
+            {
+                foreach (var sentence in Sentences)
+                {
+                    if (wordCount == sentence.Words.Count) sortedSentences.Add(sentence);
+                }
+            }
+
+            return sortedSentences.Distinct().ToList();
         }
     }
 }
